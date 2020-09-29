bd32326720c62c67b49391a593d36710815b9cbf
vahbib <singhbibhav084@gmail.com>
2020-09-29 10:32:53 +0530

    Added AddressBookMain.java File

---
+++ src/AddressBook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package AddressBook;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program in AddressBookMain class on START Master Branch");
+	}
+
+}
